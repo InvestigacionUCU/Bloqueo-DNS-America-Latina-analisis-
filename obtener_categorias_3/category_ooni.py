56f29055c32d66122f681372b9b9d140b1c2f454
RobottiFranco <franco.robotti0@gmail.com>
2025-04-22 04:24:47 -0300

    categorias ooni

--- obtener_categorias_3/category_ooni.py
+++ obtener_categorias_3/category_ooni.py
@@ -5,7 +5,7 @@ from utils.csv_utils import create_file_and_path
 from utils.fetch_data import fetch_data
 
 def cetegory(query: Query, archivo_entrada: str) -> None:
-    with open(archivo_entrada, mode='r') as file:
+    with open(archivo_entrada, mode='r', encoding='utf-8') as file:
         reader = csv.DictReader(file)
         for row in reader:
             measurement_uid = row.get('measurement_uid', None)
@@ -24,10 +24,13 @@ def cetegory(query: Query, archivo_entrada: str) -> None:
                 }
                 
                 """ escribir un un archivo salida csv con las columnas de row"""
-                output_file = create_file_and_path(archivo_entrada, "categorias_ooni")
-                
-                
-
-
+                output_file = create_file_and_path("data/lista_categorizada_ooni", "categorias_ooni.csv")
+                with open(output_file, mode='a', newline='', encoding='utf-8') as output_csv:
+                    fieldnames = ["measurement_uid", "input", "category_code"]
+                    writer = csv.DictWriter(output_csv, fieldnames=fieldnames)
+                    if output_csv.tell() == 0: 
+                        writer.writeheader()
+                    writer.writerow(row)
+                    
             except Exception as e:
                 print(f"Error al obtener datos: {e}")
